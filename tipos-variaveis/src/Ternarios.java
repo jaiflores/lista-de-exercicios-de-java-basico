4dec6ac1370059ba6dc1abd7082a2948b930db9d
jaiflores <jaianny.vivian@gmail.com>
2024-07-10 17:31:55 -0300

    Estudo basico de java

---
+++ tipos-variaveis/src/Ternarios.java
@@ -0,0 +1,27 @@
+public class Ternarios {
+
+    public static void main(String[] args) {
+        // classe Operadores.java
+        int a, b;
+
+        a = 5;
+        b = 6;
+
+        String resultado = (a==b) ? "verdadeiro" : "false";
+        System.out.println(resultado);
+    
+
+        // // EXEMPLO DE CONDICIONAL UTILIZANDO UMA ESTRUTURA IF/ELSE
+        // if(a==b)
+        //     resultado = "verdadeiro";
+        // else
+        //     resultado = "falso";
+        
+
+        // //MESMA CONDICIONAL, MAS DESSA VEZ, UTILIZANDO O OPERADOR CONDICIONAL TERNÁRIO
+        // String resultado = (a==b) ? "verdadeiro" : "false";
+
+        // System.out.println(valor);
+    }
+    
+}
