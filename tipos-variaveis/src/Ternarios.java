62d7f34ff8fc5e37446eaecd7d292bd800c5423b
jaiflores <jaianny.vivian@gmail.com>
2024-07-10 18:28:38 -0300

    Atualização de aula dos operadores

--- tipos-variaveis/src/Ternarios.java
+++ tipos-variaveis/src/Ternarios.java
@@ -1,14 +1,28 @@
 public class Ternarios {
 
     public static void main(String[] args) {
+        boolean condicao1=true;
+
+        boolean condicao2=false;
+        
+        if (condicao1 && condicao2) {
+            System.out.println("as duas condições são verdadeiras");
+        }
+        
+        if (condicao1 || condicao2) {
+            System.out.println("uma das condições é verdadeira");
+        }
+            System.out.println("fim");
+      
+      
         // classe Operadores.java
-        int a, b;
+        // int a, b;
 
-        a = 5;
-        b = 6;
+        // a = 5;
+        // b = 6;
 
-        String resultado = (a==b) ? "verdadeiro" : "false";
-        System.out.println(resultado);
+        // String resultado = (a==b) ? "verdadeiro" : "false";
+        // System.out.println(resultado);
     
 
         // // EXEMPLO DE CONDICIONAL UTILIZANDO UMA ESTRUTURA IF/ELSE
