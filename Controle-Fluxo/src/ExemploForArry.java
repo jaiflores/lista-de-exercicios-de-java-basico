d5b3d480346bfbebe5d42d2efbf853526bd46b31
jaiflores <jaianny.vivian@gmail.com>
2024-07-21 19:28:40 -0300

    Aula de repetição

---
+++ Controle-Fluxo/src/ExemploForArry.java
@@ -0,0 +1,10 @@
+public class ExemploForArry {
+    
+    public static void main(String[] args) {
+        String alunos [] = {"Fernanda", "Alice", "Bernardo"};
+        
+        for (int x=0; x<alunos.length; x++) {
+            System.out.println("O Aluno no indice x= " + x + " é " + alunos[x]);
+        }    
+    }
+}
